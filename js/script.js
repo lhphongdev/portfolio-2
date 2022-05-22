0105a159141ad3aaa8a986395931593f33d797a6
luuhoaiphong <luuhoaiphong147@gmail.com>
2022-05-22 18:23:05 +0700

    portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* typing animation */
+
+var typed = new Typed(".typing",{
+    strings:["web designer","Web Developer","Graphic Designer","Freelancer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
